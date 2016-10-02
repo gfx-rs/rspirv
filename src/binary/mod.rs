0a28f9e8e5c7e40e15683898eef6966c1221ec4d
Lei Zhang <antiagainst@gmail.com>
2016-10-02 11:34:59 -0400

    Create a separate module for SpirvOperandDecoder in binary module.

--- src/binary/mod.rs
+++ src/binary/mod.rs
@@ -2,9 +2,12 @@ pub use self::producer::Producer;
 pub use self::producer::Error as ProducerError;
 pub use self::producer::Result as ProducerResult;
 
+pub use self::decoder::OperandDecoder;
+
 pub use self::reader::Reader;
 pub use self::reader::State as ReaderState;
 pub use self::reader::Result as ReaderResult;
 
+mod decoder;
 mod producer;
 mod reader;
