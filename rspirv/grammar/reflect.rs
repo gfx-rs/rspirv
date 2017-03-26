2470dfa237614e0b7731b4dcaa7fbd498cd497f2
jonysy <jonysy@users.noreply.github.com>
2017-03-25 23:33:46 -0400

    Update reflect.rs

--- rspirv/grammar/reflect.rs
+++ rspirv/grammar/reflect.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Refect functions for SPIR-V instructions.
+//! Reflect functions for SPIR-V instructions.
 
 use spirv;
 
