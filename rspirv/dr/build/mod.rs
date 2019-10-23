62cb7cad753708a503654feb7491c63a529db171
Jasper-Bekkers <bekkers@gmail.com>
2019-10-23 13:52:49 +0200

    Bump SPIR-V version numbers in unit tests to make them pass

--- rspirv/dr/build/mod.rs
+++ rspirv/dr/build/mod.rs
@@ -898,7 +898,7 @@ mod tests {
 
         assert_eq!(
             b.module().disassemble(),
-            "; SPIR-V\n; Version: 1.3\n; Generator: rspirv\n; Bound: 11\n\
+            "; SPIR-V\n; Version: 1.4\n; Generator: rspirv\n; Bound: 11\n\
                     %1 = OpTypeVoid\n\
                     %2 = OpTypeFloat 32\n\
                     %3 = OpTypePointer Input %2\n\
@@ -945,7 +945,7 @@ mod tests {
 
         assert_eq!(
             b.module().disassemble(),
-            "; SPIR-V\n; Version: 1.3\n; Generator: rspirv\n; Bound: 9\n\
+            "; SPIR-V\n; Version: 1.4\n; Generator: rspirv\n; Bound: 9\n\
                     %1 = OpTypeVoid\n\
                     %2 = OpTypeFloat 32\n\
                     %3 = OpTypeFunction %1 %1\n\
