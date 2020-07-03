gfx-rs/rspirv

commit f726c6ff179c2df5a58febfe8d60e108e02ba018

Allow Builder to modify modules (#133)
* Create a builder from a module

* Add relative insert methods for regular instructions

* Use InsertPoint

* Try to expose selecting fns and bbs

* Fix logic

* Add helpful basic block methods

* For non-aggregate types, return the already existing id if it's already in the list of types becaues it's a validation error to have them duplicate

* Expose a reference to the internal module (read only) of the Builder

* Select a function by name for building

* Helper to find all return statements in a function

* Also handle OpReturnValue

* Update comments

* Remove un-needed mut

* Small code changes

* Move deduplication logic out into a function

autogen/dr.rs
@@ -71,6 +71,19 @@ fn get_function_name(opname: &str) -> TokenStream {
     }
 }
 
+/// Returns a suitable function name for the given `opname`.
+fn get_function_name_with_prepend(prepend: &str, opname: &str) -> TokenStream {
+    let name = if opname == "OpReturn" {
+        as_ident(&format!("{}ret", prepend))
+    } else if opname == "OpReturnValue" {
+        as_ident(&format!("{}ret_value", prepend))
+    } else {
+        as_ident(&format!("{}{}", prepend, &opname[2..].to_snake_case()))
+    };
+
+    quote! { #name }
+}
+
 /// Returns the initializer list for all the parameters required to appear
 /// once and only once.
 fn get_init_list(params: &[structs::Operand]) -> Vec<TokenStream> {
@@ -272,16 +285,36 @@ pub fn gen_dr_builder_types(grammar: &structs::Grammar) -> TokenStream {
                                      quote! { self.module.types_global_values.last_mut().expect("interal error").operands });
         let opcode = as_ident(&inst.opname[2..]);
         let name = as_ident(&inst.opname[2..].to_snake_case());
-        let comment = format!("Appends an Op{} instruction and returns the result id.", opcode);
-        quote! {
-            #[doc = #comment]
-            pub fn #name#generic(&mut self,#(#param_list),*) -> spirv::Word {
-                let id = self.id();
-                self.module.types_global_values.push(
-                    dr::Instruction::new(spirv::Op::#opcode, None, Some(id), vec![#(#init_list),*])
-                );
-                #(#extras)*
-                id
+
+        let aggregate = match inst.opname.as_ref() {
+            "SpvOpTypeArray" | "SpvOpTypeRuntimeArray" | "SpvOpTypePointer" | "SpvOpTypeStruct" => true,
+            _ => false
+        };
+
+        if aggregate {
+            let comment = format!("Appends an Op{} instruction and returns the result id.", opcode);
+            quote! {
+                #[doc = #comment]
+                pub fn #name#generic(&mut self,#(#param_list),*) -> spirv::Word {
+                    let id = self.id();
+                    self.module.types_global_values.push(
+                        dr::Instruction::new(spirv::Op::#opcode, None, Some(id), vec![#(#init_list),*])
+                    );
+                    #(#extras)*
+                    id
+                }
+            }
+        } else {
+            let comment = format!("Appends an Op{} instruction and returns the result id, or return the existing id if the instruction was already present.", opcode);
+            quote! {
+                #[doc = #comment]
+                pub fn #name#generic(&mut self,#(#param_list),*) -> spirv::Word {
+                    let mut inst = dr::Instruction::new(spirv::Op::#opcode, None, None, vec![#(#init_list),*]);
+                    inst.result_id = Some(self.dedup_insert_type(&inst));
+                    self.module.types_global_values.push(inst);
+                    #(#extras)*
+                    id
+                }
             }
         }
     });
@@ -298,12 +331,20 @@ pub fn gen_dr_builder_terminator(grammar: &structs::Grammar) -> TokenStream {
     let elements = grammar.instructions.iter().filter(|inst| {
         inst.class == Some(structs::Class::Terminator) || inst.class == Some(structs::Class::Branch)
     }).map(|inst| {
-        let (params, generic) = get_param_list(&inst.operands, false, kinds);
+        let (params, generic) = get_param_list(&inst.operands, inst.opname == "OpLabel", kinds);
         let extras = get_push_extras(&inst.operands, kinds, quote! { inst.operands });
         let opcode = as_ident(&inst.opname[2..]);
         let comment = format!("Appends an Op{} instruction and ends the current block.", opcode);
+        let insert_comment = format!("Insert an Op{} instruction and ends the current block.", opcode);
         let name = get_function_name(&inst.opname);
         let init = get_init_list(&inst.operands);
+        let insert_name = get_function_name_with_prepend("insert_", &inst.opname);
+
+        let result_id = if inst.opname == "OpLabel" {
+            quote! { result_id }
+        } else {
+            quote! { None }
+        };
 
         let result_type = if inst.opname == "OpPhi" {
             quote! { Some(result_type) }
@@ -316,9 +357,18 @@ pub fn gen_dr_builder_terminator(grammar: &structs::Grammar) -> TokenStream {
             pub fn #name#generic(&mut self,#(#params),*) -> BuildResult<()> {
                 #[allow(unused_mut)]
                 let mut inst = dr::Instruction::new(
-                    spirv::Op::#opcode, #result_type, None, vec![#(#init),*]);
+                    spirv::Op::#opcode, #result_type, #result_id, vec![#(#init),*]);
                 #(#extras)*
                 self.end_block(inst)
+            }
+
+            #[doc = #insert_comment]
+            pub fn #insert_name#generic(&mut self, insert_point: InsertPoint, #(#params),*) -> BuildResult<()> {
+                #[allow(unused_mut)]
+                let mut inst = dr::Instruction::new(
+                    spirv::Op::#opcode, #result_type, #result_id, vec![#(#init),*]);
+                #(#extras)*
+                self.insert_end_block(insert_point, inst)
             }
         }
     });
@@ -361,7 +411,9 @@ pub fn gen_dr_builder_normal_insts(grammar: &structs::Grammar) -> TokenStream {
         let extras = get_push_extras(&inst.operands, kinds, quote! { inst.operands });
         let opcode = as_ident(&inst.opname[2..]);
         let comment = format!("Appends an Op{} instruction to the current block.", opcode);
+        let insert_comment = format!("Appends an Op{} instruction to the current block.", opcode);
         let name = get_function_name(&inst.opname);
+        let insert_name = get_function_name_with_prepend("insert_", &inst.opname);
         let init = get_init_list(&inst.operands);
 
         if !inst.operands.is_empty() && inst.operands[0].kind == "IdResultType" {
@@ -370,9 +422,6 @@ pub fn gen_dr_builder_normal_insts(grammar: &structs::Grammar) -> TokenStream {
             quote! {
                 #[doc = #comment]
                 pub fn #name#generic(&mut self,#(#params),*) -> BuildResult<spirv::Word> {
-                    if self.block.is_none() {
-                        return Err(Error::DetachedInstruction);
-                    }
                     let _id = match result_id {
                         Some(v) => v,
                         None => self.id(),
@@ -381,7 +430,21 @@ pub fn gen_dr_builder_normal_insts(grammar: &structs::Grammar) -> TokenStream {
                     let mut inst = dr::Instruction::new(
                         spirv::Op::#opcode, Some(result_type), Some(_id), vec![#(#init),*]);
                     #(#extras)*
-                    self.block.as_mut().unwrap().instructions.push(inst);
+                    self.insert_into_block(InsertPoint::End, inst)?;
+                    Ok(_id)
+                }
+
+                #[doc = #insert_comment]
+                pub fn #insert_name#generic(&mut self,insert_point: InsertPoint, #(#params),*) -> BuildResult<spirv::Word> {
+                    let _id = match result_id {
+                        Some(v) => v,
+                        None => self.id(),
+                    };
+                    #[allow(unused_mut)]
+                    let mut inst = dr::Instruction::new(
+                        spirv::Op::#opcode, Some(result_type), Some(_id), vec![#(#init),*]);
+                    #(#extras)*
+                    self.insert_into_block(insert_point, inst)?;
                     Ok(_id)
                 }
             }
@@ -389,14 +452,21 @@ pub fn gen_dr_builder_normal_insts(grammar: &structs::Grammar) -> TokenStream {
             quote! {
                 #[doc = #comment]
                 pub fn #name#generic(&mut self,#(#params),*) -> BuildResult<()> {
-                    if self.block.is_none() {
-                        return Err(Error::DetachedInstruction);
-                    }
                     #[allow(unused_mut)]
                     let mut inst = dr::Instruction::new(
                         spirv::Op::#opcode, None, None, vec![#(#init),*]);
                     #(#extras)*
-                    self.block.as_mut().unwrap().instructions.push(inst);
+                    self.insert_into_block(InsertPoint::End, inst)?;
+                    Ok(())
+                }
+
+                #[doc = #comment]
+                pub fn #insert_name#generic(&mut self,insert_point: InsertPoint, #(#params),*) -> BuildResult<()> {
+                    #[allow(unused_mut)]
+                    let mut inst = dr::Instruction::new(
+                        spirv::Op::#opcode, None, None, vec![#(#init),*]);
+                    #(#extras)*
+                    self.insert_into_block(insert_point, inst)?;
                     Ok(())
                 }
             }

commit 9eed542c8ac0f5b7dd9cec34230ff2d4bd0fdea5

dr: Re-add missing id in builder_types
The assignment of id for non-aggregate types is missing, resulting in
build errors when compiling autogen_type.rs.

The desired id to return is not self.id(), but the result of
dedup_insert_type which returns an existing id or self.id() if not
found.

Fixes: f726c6ff179c2df5a58febfe8d60e108e02ba018

autogen/dr.rs
@@ -310,7 +310,8 @@ pub fn gen_dr_builder_types(grammar: &structs::Grammar) -> TokenStream {
                 #[doc = #comment]
                 pub fn #name#generic(&mut self,#(#param_list),*) -> spirv::Word {
                     let mut inst = dr::Instruction::new(spirv::Op::#opcode, None, None, vec![#(#init_list),*]);
-                    inst.result_id = Some(self.dedup_insert_type(&inst));
+                    let id = self.dedup_insert_type(&inst);
+                    inst.result_id = Some(id);
                     self.module.types_global_values.push(inst);
                     #(#extras)*
                     id

rspirv/dr/build/autogen_type.rs
@@ -6,14 +6,16 @@ impl Builder {
     #[doc = "Appends an OpTypeVoid instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_void(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeVoid, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypeBool instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_bool(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeBool, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -28,7 +30,8 @@ impl Builder {
                 dr::Operand::LiteralInt32(signedness),
             ],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -40,7 +43,8 @@ impl Builder {
             None,
             vec![dr::Operand::LiteralInt32(width)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -59,7 +63,8 @@ impl Builder {
                 dr::Operand::LiteralInt32(component_count),
             ],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -74,7 +79,8 @@ impl Builder {
                 dr::Operand::LiteralInt32(column_count),
             ],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -104,7 +110,8 @@ impl Builder {
                 dr::Operand::ImageFormat(image_format),
             ],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         if let Some(v) = access_qualifier {
             #[allow(clippy::identity_conversion)]
@@ -120,7 +127,8 @@ impl Builder {
     #[doc = "Appends an OpTypeSampler instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_sampler(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeSampler, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -132,7 +140,8 @@ impl Builder {
             None,
             vec![dr::Operand::IdRef(image_type)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -144,7 +153,8 @@ impl Builder {
             None,
             vec![dr::Operand::IdRef(element_type), dr::Operand::IdRef(length)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -156,7 +166,8 @@ impl Builder {
             None,
             vec![dr::Operand::IdRef(element_type)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -166,7 +177,8 @@ impl Builder {
         member_0_type_member_1_type: T,
     ) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeStruct, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         for v in member_0_type_member_1_type.as_ref() {
             self.module
@@ -190,7 +202,8 @@ impl Builder {
             None,
             vec![dr::Operand::IdRef(return_type)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         for v in parameter_0_type_parameter_1_type.as_ref() {
             self.module
@@ -205,28 +218,32 @@ impl Builder {
     #[doc = "Appends an OpTypeEvent instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_event(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeEvent, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypeDeviceEvent instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_device_event(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeDeviceEvent, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypeReserveId instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_reserve_id(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeReserveId, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypeQueue instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_queue(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeQueue, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
@@ -238,21 +255,24 @@ impl Builder {
             None,
             vec![dr::Operand::AccessQualifier(qualifier)],
         );
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypePipeStorage instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_pipe_storage(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypePipeStorage, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }
     #[doc = "Appends an OpTypeNamedBarrier instruction and returns the result id, or return the existing id if the instruction was already present."]
     pub fn type_named_barrier(&mut self) -> spirv::Word {
         let mut inst = dr::Instruction::new(spirv::Op::TypeNamedBarrier, None, None, vec![]);
-        inst.result_id = Some(self.dedup_insert_type(&inst));
+        let id = self.dedup_insert_type(&inst);
+        inst.result_id = Some(id);
         self.module.types_global_values.push(inst);
         id
     }